d0aa27512673db09aaaa58f857de4efa5ade7330
Maya™ <76062544+meetmaya@users.noreply.github.com>
2021-12-03 22:35:37 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,36 +2,28 @@ import os
 from dotenv import load_dotenv
 import openai
 
-
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.Completion()
 
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
+start_chat_log = '''Human: Hello, who are you?
+AI: I am doing great. How can I help you today?
+'''
 
-response = openai.Completion.create(
-  engine="davinci",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=["\n", " Human:", " AI:"]
-)
 
 def ask(question, chat_log=None):
- prompt_text = f’{chat_log}{restart_sequence}: {question}{start_sequence}:’
- response = openai.Completion.create(
- engine=”davinci”,
- prompt=prompt_text,
- temperature=0.8,
- max_tokens=150,
- top_p=1,
- frequency_penalty=0,
- presence_penalty=0.3,
- stop=[“\n”],
- )
- story = response[‘choices’][0][‘text’]
- return str(story)
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
+
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    return f'{chat_log}Human: {question}\nAI: {answer}\n'
