1471e5337080769d2fc67774864a10ea3c971e5e
Maya™ <76062544+meetmaya@users.noreply.github.com>
2021-12-03 22:26:37 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,42 @@
-import os
 from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
+import os
 import openai
 
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.Completion()
 
-start_chat_log = '''Human: Hello, who are you?
-AI: I am doing great. How can I help you today?
-'''
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
 
+response = openai.Completion.create(
+  engine="davinci",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=["\n", " Human:", " AI:"]
+)
 
 def ask(question, chat_log=None):
-    if chat_log is None:
-        chat_log = start_chat_log
-    prompt = f'{chat_log}Human: {question}\nAI:'
-    response = completion.create(
-        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-        max_tokens=150)
-    answer = response.choices[0].text.strip()
-    return answer
+ prompt_text = f’{chat_log}{restart_sequence}: {question}{start_sequence}:’
+ response = openai.Completion.create(
+ engine=”davinci”,
+ prompt=prompt_text,
+ temperature=0.8,
+ max_tokens=150,
+ top_p=1,
+ frequency_penalty=0,
+ presence_penalty=0.3,
+ stop=[“\n”],
+ )
+ story = response[‘choices’][0][‘text’]
+ return str(story)
 
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
-    if chat_log is None:
-        chat_log = start_chat_log
-    return f'{chat_log}Human: {question}\nAI: {answer}\n'
+if chat_log is None: chat_log = session_prompt return f’{chat_log}{restart_sequence} {question}{start_sequence}{answer}’
