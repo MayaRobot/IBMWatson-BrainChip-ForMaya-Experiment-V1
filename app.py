d66e8c57ee7443a29ec4cc941b486b321732ae7a
Maya™ <76062544+meetmaya@users.noreply.github.com>
2021-12-04 10:31:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ from twilio.twiml.messaging_response import MessagingResponse
 from chatbot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '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'
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    incoming_msg = request.values['Body']
+    chat_log = session.get('chat_log')
+
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                         chat_log)
+
+    r = MessagingResponse()
+    r.message(answer)
+    return str(r)
