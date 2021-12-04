4549593b2c5a6ecc4bab9f2ecfd6e07017fdce3b
Maya™ <76062544+meetmaya@users.noreply.github.com>
2021-12-04 11:27:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from twilio.twiml.messaging_response import MessagingResponse
 from chatbot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
+# if for some reason your conversation with the bot gets weird, change the secret key 
 app.config['SECRET_KEY'] = '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'
 
-
-@app.route('/bot', methods=['POST'])
+@app.route('/chatbot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
     chat_log = session.get('chat_log')
-
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
+    msg = MessagingResponse()
+    msg.message(answer)
+    return str(msg)
 
-    r = MessagingResponse()
-    r.message(answer)
-    return str(r)
+if __name__ == '__main__':
+    app.run(debug=True)
