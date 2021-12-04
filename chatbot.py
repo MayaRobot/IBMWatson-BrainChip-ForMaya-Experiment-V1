2ebe104da21dbe76cf3ce1b7f187075e56435f9d
Maya™ <76062544+meetmaya@users.noreply.github.com>
2021-12-03 22:31:38 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,7 @@
-from dotenv import load_dotenv
-from random import choice
-from flask import Flask, request
 import os
+from dotenv import load_dotenv
 import openai
+
 
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
@@ -36,7 +35,3 @@ def ask(question, chat_log=None):
  )
  story = response[‘choices’][0][‘text’]
  return str(story)
-
-
-def append_interaction_to_chat_log(question, answer, chat_log=None):
-if chat_log is None: chat_log = session_prompt return f’{chat_log}{restart_sequence} {question}{start_sequence}{answer}’
